1b8d7679fbfac24e7d6ca960b7f03b8f836ba469
Sergei Demchuk <vinci.ru@mail.ru>
2021-08-11 15:05:37 +0300

    refactor: move checkbox text

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -48,6 +48,7 @@ export default function Form({ setFormData }) {
     if (deliveryDate === '') {
       setErrors((curErrors) => ({ ...curErrors, deliveryDate }));
     }
+    console.log(errors.firstName);
   };
 
   const resetFields = () => {
